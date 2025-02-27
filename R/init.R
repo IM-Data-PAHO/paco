1fe72187334efffab22341d94c8c39c95cfc57d6
danalvarez <d.alvrzx@gmail.com>
2025-02-27 17:20:44 -0600

    Improve init (addresses #2)

--- R/init.R
+++ R/init.R
@@ -1,19 +1,16 @@
-#' Initialize a new RStudio project with a particular directory structure.
+#' Create a directory structure according to PAHO/CIM standards.
 #'
-#' @param project_name The name of the project (used in the README.txt and .Rproj file)
-#' @param path The path where the project directory should be created
+#' @param project_name The name of the project (used in the README.txt file). Defaults to "Insert project name here".
+#' @param path The path where the project directory should be created. Defaults to current working directory.
 #' @return NULL
 #' @export
-init <- function(project_name, path = ".") {
-  # Create the main project directory
-  dir.create(file.path(path, project_name), showWarnings = FALSE)
-  
+init <- function(project_name = "Insert project name here", path = ".") {
   # Define the folder structure
   folders <- c("scripts", "output", "docs", "data", "styles")
   
   # Create the folders
   for (folder in folders) {
-    dir.create(file.path(path, project_name, folder), showWarnings = FALSE)
+    dir.create(file.path(path, folder), showWarnings = FALSE)
   }
   
   # Path to the templates
@@ -28,21 +25,16 @@ init <- function(project_name, path = ".") {
   readme_content <- readLines(readme_template)
   readme_content <- gsub("<project_name>", project_name, readme_content)
   readme_content <- gsub("<creation_date>", Sys.Date(), readme_content)
-  writeLines(readme_content, file.path(path, project_name, "README.txt"))
+  writeLines(readme_content, file.path(path, "README.txt"))
   
   # Copy the requirements.R template
   requirements_template <- file.path(template_dir, "requirements_template.R")
-  file.copy(requirements_template, file.path(path, project_name, "requirements.R"))
+  file.copy(requirements_template, file.path(path, "requirements.R"))
   
   # Copy the PAHO_styles.R template
   PAHO_styles_template <- file.path(template_dir, "PAHO_styles_template.R")
-  file.copy(PAHO_styles_template, file.path(path, project_name, "/styles/PAHO_styles.R"))
+  file.copy(PAHO_styles_template, file.path(path, "/styles/PAHO_styles.R"))
   
-  # Copy and rename the .Rproj template
-  rproj_template <- file.path(template_dir, "RStudio_project_template.Rproj")
-  rproj_destination <- file.path(path, project_name, paste0(project_name, ".Rproj"))
-  file.copy(rproj_template, rproj_destination)
-  
-  message("Project directory initialized successfully.")
+  message("Directory initialized successfully.")
   invisible(NULL)
 }
